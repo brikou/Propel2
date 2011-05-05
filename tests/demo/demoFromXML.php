a21edbe0731795dab6b54d145df42a5a46eda2eb
Brikou CARRE <brikou@gmail.com>
2011-05-05 19:44:59 +0200

    fixes + tweaks

--- tests/demo/demoFromXML.php
+++ tests/demo/demoFromXML.php
@@ -8,7 +8,7 @@ $dir     = __DIR__ . '/Model/xml/' . $project;
 echo shell_exec(' php /var/www/' . $project . '/app/console doctrine:mapping:convert --force xml ' . $dir);
 */
 
-$dir = '/var/www/AcmePizza/src/Acme/PizzaBundle/Resources/config/';
+$dir = '/var/www/AcmePizza/src/Acme/PizzaBundle/Resources/config/doctrine/';
 
 require_once __DIR__ . '/../../autoload.php';
 
@@ -21,25 +21,50 @@ use Propel\Builder\Generator;
 
 $config = new Configuration();
 $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
+
 $driverImpl = new XmlDriver($dir);
+$driverImpl->setFileExtension('.orm.dcm.xml');
 $config->setMetadataDriverImpl($driverImpl);
 
 $config->setProxyDir(__DIR__ . '/Proxies');
 $config->setProxyNamespace('Proxies');
 
-$connectionOptions = array(
+$cmf = new DisconnectedClassMetadataFactory();
+$cmf->setEntityManager(\Doctrine\ORM\EntityManager::create(array(
     'driver' => 'pdo_sqlite',
     'path' => 'database.sqlite'
-);
-
-$em = \Doctrine\ORM\EntityManager::create($connectionOptions, $config);
-$cmf = new DisconnectedClassMetadataFactory();
-$cmf->setEntityManager($em);
+), $config));
 
 $generator = new Generator();
+
 foreach ($cmf->getAllMetadata() as $metadata) {
-    $generator->addBuilder(new BaseActiveRecord($metadata));
-    $generator->addBuilder(new ActiveRecord($metadata));
+
+    /* @var $metadata Doctrine\ORM\Mapping\ClassMetadataInfo */
+
+    /* identifiers are put on top of fieldMappings */
+    $identifiers = array();
+    $positions   = array();
+
+    $i = 0;
+    foreach ($metadata->fieldMappings as $fieldMapping) {
+        $positions[]   = $i;
+        $identifiers[] = isset($fieldMapping['id']) ? 1 : 0;
+        $i++;
+    }
+
+    array_multisort(
+        $identifiers, SORT_DESC, SORT_NUMERIC,
+        $positions, SORT_ASC, SORT_NUMERIC,
+        $metadata->fieldMappings
+    );
+    
+    $builder = new BaseActiveRecord($metadata);
+
+    $builder->setMappingDriver(BaseActiveRecord::MAPPING_ANNOTATION);
+    $builder->setAnnotationPrefix('orm');
+
+    $generator->addBuilder($builder);
+    if (false)$generator->addBuilder(new ActiveRecord($metadata));
 }
 echo "Generating classes for xml schemas...\n";
 $generator->writeClasses(__DIR__ . '/Model');
