83a8d0a23a49c060a44b1371d286f20c79cc7df2
Brikou CARRE <brikou@gmail.com>
2011-05-08 08:05:26 +0200

    moved typeToPhp to Utils

--- src/Propel/Builder/TwigBuilder.php
+++ src/Propel/Builder/TwigBuilder.php
@@ -15,7 +15,7 @@ class TwigBuilder
         'substr',
         '\Propel\Util\ArrayType::stringify',
         '\Propel\Util\Inflector::singularize',
-        '\Propel\Builder\TwigBuilder::typeToPhp',
+        '\Propel\Util\ORM::typeToPhp',
     );
     protected $variables = array();
     protected $tempDir;
@@ -144,34 +144,5 @@ class TwigBuilder
             }
             $twig->addFilter($twigFilterName, new \Twig_Filter_Function($twigFilter));
         }
-    }
-
-    /**
-     * @see http://www.doctrine-project.org/docs/orm/2.0/en/reference/basic-mapping.html#doctrine-mapping-types
-     * @see http://www.php.net/manual/en/language.types.intro.php
-     * 
-     * @param string $type
-     * 
-     * @return string
-     */
-    static public function typeToPhp($type)
-    {
-        $types = array(
-            'string'   => 'string',
-            'integer'  => 'integer',
-            'smallint' => 'integer',
-            'bigint'   => 'string',
-            'boolean'  => 'boolean',
-            'decimal'  => 'float',
-            'date'     => '\DateTime',
-            'time'     => '\DateTime',
-            'datetime' => '\DateTime',
-            'text'     => 'string',
-            'object'   => 'object',
-            'array'    => 'object',
-            'float'    => 'float',
-        );
-
-        return $types[$type];
     }
 }
